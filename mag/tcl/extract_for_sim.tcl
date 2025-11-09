ff80243f7f8775ef4a9898bab866ac63c864aaae
Anton Maurovic <anton@foogle.com>
2025-11-09 16:05:15 +1030

    'make sim_spice_from_gds' plus power port ties in top GDS to help sim PEX extraction

--- mag/tcl/extract_for_sim.tcl
+++ mag/tcl/extract_for_sim.tcl
@@ -8,10 +8,10 @@ cellname rename tt_um_flat ${project}_parax
 extract all
 ext2sim labels on
 ext2sim
-#extresist tolerance 0.10 ; # Merge more resistors (normally 10%, pushing up to 20% for simpler netlist)
+#extresist tolerance 0.10 ; # Merge more resistors (normally 10, to 20 for simpler netlist)
 extresist
 ext2spice lvs
-ext2spice cthresh 0.01 ; # Ignore caps below 1e-18 (normally 0, this should cut out ~36% of caps)
+ext2spice cthresh 0.01 ; # Ignore caps below 1e-17 (normally 0, this should cut out ~36% of caps)
 ext2spice extresist on
 ext2spice -o $project.sim.spice
 quit -noprompt
---
+++ mag/tcl/extract_from_gds_for_sim.tcl
@@ -0,0 +1,21 @@
+# This borrows from extract_for_sim.tcl to generate an extraction from a .gds source rather than .mag file.
+# It is expected this will be called by mag/Makefile, which will change to the ../gds/
+# directory, but write the resulting SPICE to ../sim/spice/
+set project [lindex $argv $argc-1]
+gds read $project.gds
+cd ../sim/spice
+flatten tt_um_flat
+load tt_um_flat
+select top cell
+cellname delete $project
+cellname rename tt_um_flat ${project}_parax
+extract all
+ext2sim labels on
+ext2sim
+#extresist tolerance 0.10 ; # Merge more resistors (normally 10%, pushing up to 20% for simpler netlist)
+extresist
+ext2spice lvs
+ext2spice cthresh 0.01 ; # Ignore caps below 1e-18 (normally 0, this should cut out ~36% of caps)
+ext2spice extresist on
+ext2spice -o ../sim/spice/$project.from_gds.sim.spice
+quit -noprompt
